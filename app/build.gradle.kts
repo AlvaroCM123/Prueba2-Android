2450c83b13055d595b714798a8a8267a0b24f15d
AlvaroCM123 <a.cardenas56@alumnos.santotomas.cl>
2025-10-14 16:34:14 -0300

    Integracion basica del mapa en la app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/pruebaandroid2/MainActivity.kt
+++ app/src/main/java/com/example/pruebaandroid2/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.pruebaandroid2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.preference.PreferenceManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        mapView = findViewById(R.id.map)
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+        mapView.setMultiTouchControls(true)
+
+        val mapController = mapView.controller
+        mapController.setZoom(16.0)
+        val startPoint = GeoPoint(-33.4430, -70.6533)
+        mapController.setCenter(startPoint)
+
+        val startMarker = Marker(mapView)
+        startMarker.position = startPoint
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        startMarker.title = "Aqui estoy"
+        mapView.overlays.add(startMarker)
+
+        mapView.invalidate()
+    }
+    override fun onResume() {
+            super.onResume()
+            mapView.onResume()
+        }
+
+    override fun onPause() {
+            super.onPause()
+            mapView.onPause()
         }
     }
-}
