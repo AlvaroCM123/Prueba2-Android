a8b15a8a0fcaccd33955f5afbde07badd2f1405f
AlvaroCM123 <a.cardenas56@alumnos.santotomas.cl>
2025-10-30 17:17:06 -0300

    Se creo el diseño completo del login(todavia sin funcianalidad)

--- app/src/main/java/com/example/pruebaandroid2/LoginActivity.kt
+++ app/src/main/java/com/example/pruebaandroid2/LoginActivity.kt
@@ -5,16 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Base_Theme_PruebaAndroid2)
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Integracion de Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
     }
+
 }
+//ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//insets
